d9c83941b571e02eb096c22615bbc211419b2c32
Yonas Hiko <yonashumna@gmail.com>
2021-05-05 22:13:52 -0400

    first_exercise

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+console.disableYellowBox = true;
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Main />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
